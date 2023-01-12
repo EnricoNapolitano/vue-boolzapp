3a0f897d10f60a86c8192f6a9e9589d6336247a4
EnricoNapolitano <enericonapolitano.ita@gmail.com>
2023-01-12 21:28:44 +0100

    contacts list generated by v-for

--- js/script.js
+++ js/script.js
@@ -1,5 +1,32 @@
 console.log('ok ' + Vue);
 
-const app = Vue.createApp();
+const app = Vue.createApp({
+    data(){
+        return {
+            user: {
+                name: 'Nome Utente',
+                avatar: '_io'
+              },
+              contacts: [
+                {
+                  name: 'Michele',
+                  avatar: '_1',
+                },
+                {
+                  name: 'Fabio',
+                  avatar: '_2',
+                },
+                {
+                  name: 'Samuele',
+                  avatar: '_3',
+                },
+                {
+                  name: 'Luisa',
+                  avatar: '_4',
+                },
+              ]
+        }
+    }
+});
 
 app.mount('#ui');
